eb0609b9258479f9e1c71d39ed9635c0d0a98f15
zhouxiao123 <919486216@qq.com>
2018-03-30 17:32:47 +0800

    初次上传

--- src/main/java/com/safety/NettyWebSocket/MyWebSocketServerHandler.java
+++ src/main/java/com/safety/NettyWebSocket/MyWebSocketServerHandler.java
@@ -2,16 +2,20 @@ package com.safety.NettyWebSocket;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.*;
 import io.netty.handler.codec.http.*;
 import io.netty.handler.codec.http.websocketx.*;
+import io.netty.handler.ssl.SslHandler;
+import io.netty.handler.stream.ChunkedNioFile;
 import io.netty.util.AttributeKey;
 import io.netty.util.CharsetUtil;
+import org.springframework.web.HttpRequestHandler;
 
 
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -21,179 +25,73 @@ import java.util.logging.Logger;
 /**
  * Created by Administrator on 2018/3/30.
  */
-public class MyWebSocketServerHandler extends SimpleChannelInboundHandler<Object> {
-    private static final Logger logger = Logger.getLogger(WebSocketServerHandshaker.class.getName());
-    private WebSocketServerHandshaker handshaker;
-    /**
-     * channel 通道 action 活跃的 当客户端主动链接服务端的链接后，这个通道就是活跃的了。也就是客户端与服务端建立了通信通道并且可以传输数据
-     */
-    @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-// 添加
-        Global.group.add(ctx.channel());
-        System.out.println("客户端与服务端连接开启：" + ctx.channel().remoteAddress().toString());
-        //Global.group.writeAndFlush("进场");
-    }
-    /**
-     * channel 通道 Inactive 不活跃的 当客户端主动断开服务端的链接后，这个通道就是不活跃的。也就是说客户端与服务端关闭了通信通道并且不可以传输数据
-     */
-    @Override
-    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-// 移除
-        Global.group.remove(ctx.channel());
-        System.out.println("客户端与服务端连接关闭：" + ctx.channel().remoteAddress().toString());
-    }
-    /**
-     * 接收客户端发送的消息 channel 通道 Read 读 简而言之就是从通道中读取数据，也就是服务端接收客户端发来的数据。但是这个数据在不进行解码时它是ByteBuf类型的
-     */
-    /*@Override
-    protected void messageReceived(ChannelHandlerContext ctx, Object msg) throws Exception {
-// 传统的HTTP接入
-        if (msg instanceof FullHttpRequest) {
-            handleHttpRequest(ctx, ((FullHttpRequest) msg));
-// WebSocket接入
-        } else if (msg instanceof WebSocketFrame) {
-            System.out.println(handshaker.uri());
-            if("anzhuo".equals(ctx.attr(AttributeKey.valueOf("type")).get())){
-                handlerWebSocketFrame(ctx, (WebSocketFrame) msg);
-            }else{
-                handlerWebSocketFrame2(ctx, (WebSocketFrame) msg);
-            }
-        }
-    }*/
-    /**
-     * channel 通道 Read 读取 Complete 完成 在通道读取完成后会在这个方法里通知，对应可以做刷新操作 ctx.flush()
-     */
-    @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        ctx.flush();
-    }
-    private void handlerWebSocketFrame(ChannelHandlerContext ctx, WebSocketFrame frame) {
-// 判断是否关闭链路的指令
-        if (frame instanceof CloseWebSocketFrame) {
-            System.out.println(1);
-            handshaker.close(ctx.channel(), (CloseWebSocketFrame) frame.retain());
-            return;
-        }
-// 判断是否ping消息
-        if (frame instanceof PingWebSocketFrame) {
-            ctx.channel().write(new PongWebSocketFrame(frame.content().retain()));
-            return;
-        }
-// 本例程仅支持文本消息，不支持二进制消息
-        if (!(frame instanceof TextWebSocketFrame)) {
-            System.out.println("本例程仅支持文本消息，不支持二进制消息");
-            throw new UnsupportedOperationException(
-                    String.format("%s frame types not supported", frame.getClass().getName()));
-        }
-// 返回应答消息
-        String request = ((TextWebSocketFrame) frame).text();
-        System.out.println("服务端收到：" + request);
-        if (logger.isLoggable(Level.FINE)) {
-            logger.fine(String.format("%s received %s", ctx.channel(), request));
-        }
-        TextWebSocketFrame tws = new TextWebSocketFrame(new Date().toString() + ctx.channel().id() + "：" + request);
-// 群发
-        Global.group.writeAndFlush(tws);
-// 返回【谁发的发给谁】
-// ctx.channel().writeAndFlush(tws);
-    }
-    private void handlerWebSocketFrame2(ChannelHandlerContext ctx, WebSocketFrame frame) {
-// 判断是否关闭链路的指令
-        if (frame instanceof CloseWebSocketFrame) {
-            handshaker.close(ctx.channel(), (CloseWebSocketFrame) frame.retain());
-            return;
-        }
-// 判断是否ping消息
-        if (frame instanceof PingWebSocketFrame) {
-            ctx.channel().write(new PongWebSocketFrame(frame.content().retain()));
-            return;
-        }
-// 本例程仅支持文本消息，不支持二进制消息
-        if (!(frame instanceof TextWebSocketFrame)) {
-            System.out.println("本例程仅支持文本消息，不支持二进制消息");
-            throw new UnsupportedOperationException(
-                    String.format("%s frame types not supported", frame.getClass().getName()));
-        }
-// 返回应答消息
-        String request = ((TextWebSocketFrame) frame).text();
-        System.out.println("服务端2收到：" + request);
-        if (logger.isLoggable(Level.FINE)) {
-            logger.fine(String.format("%s received %s", ctx.channel(), request));
-        }
-        TextWebSocketFrame tws = new TextWebSocketFrame(new Date().toString() + ctx.channel().id() + "：" + request);
-// 群发
-        Global.group.writeAndFlush(tws);
-// 返回【谁发的发给谁】
-// ctx.channel().writeAndFlush(tws);
-    }
-    private void handleHttpRequest(ChannelHandlerContext ctx, FullHttpRequest req) {
-// 如果HTTP解码失败，返回HHTP异常
-        if (!req.decoderResult().isSuccess() || (!"websocket".equals(req.headers().get("Upgrade")))) {
-            sendHttpResponse(ctx, req,
-                    new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.BAD_REQUEST));
-            return;
-        }
-//获取url后置参数
-        HttpMethod method=req.method();
-        String uri=req.uri();
-        QueryStringDecoder queryStringDecoder = new QueryStringDecoder(uri);
-        Map<String, List<String>> parameters = queryStringDecoder.parameters();
-        System.out.println(parameters.get("request").get(0));
-        if(method==HttpMethod.GET&&"/webssss".equals(uri)){
-//....处理
+public class MyWebSocketServerHandler extends SimpleChannelInboundHandler<FullHttpRequest> { //1
+    private final String wsUri;
+    private static final File INDEX;
 
-            ctx.attr(AttributeKey.valueOf("type")).set("anzhuo");
-        }else if(method==HttpMethod.GET&&"/websocket".equals(uri)){
-//...处理
-            ctx.attr(AttributeKey.valueOf("type")).set("live");
-        }
-// 构造握手响应返回，本机测试
-        WebSocketServerHandshakerFactory wsFactory = new WebSocketServerHandshakerFactory(
-                "ws://"+req.headers().get(HttpHeaderNames.HOST)+uri, null, false);
-        handshaker = wsFactory.newHandshaker(req);
-        if (handshaker == null) {
-            WebSocketServerHandshakerFactory.sendUnsupportedVersionResponse(ctx.channel());
-        } else {
-            handshaker.handshake(ctx.channel(), req);
+    static {
+        URL location = HttpRequestHandler.class.getProtectionDomain().getCodeSource().getLocation();
+        try {
+            String path = location.toURI() + "WebsocketChatClient.html";
+            path = !path.contains("file:") ? path : path.substring(5);
+            INDEX = new File(path);
+        } catch (URISyntaxException e) {
+            throw new IllegalStateException("Unable to locate WebsocketChatClient.html", e);
         }
     }
-    private static void sendHttpResponse(ChannelHandlerContext ctx, FullHttpRequest req, DefaultFullHttpResponse res) {
-// 返回应答给客户端
-        if (res.status().code() != 200) {
-            ByteBuf buf = Unpooled.copiedBuffer(res.status().toString(), CharsetUtil.UTF_8);
-            res.content().writeBytes(buf);
-            buf.release();
-        }
-// 如果是非Keep-Alive，关闭连接
-        ChannelFuture f = ctx.channel().writeAndFlush(res);
-        if (!HttpUtil.isKeepAlive(req) || res.status().code() != 200) {
-            f.addListener(ChannelFutureListener.CLOSE);
-        }
+
+    public MyWebSocketServerHandler(String wsUri) {
+        this.wsUri = wsUri;
     }
-    /**
-     * exception 异常 Caught 抓住 抓住异常，当发生异常的时候，可以做一些相应的处理，比如打印日志、关闭链接
-     */
+
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+    public void channelRead0(ChannelHandlerContext ctx, FullHttpRequest request) throws Exception {
+        if (wsUri.equalsIgnoreCase(request.getUri())) {
+            ctx.fireChannelRead(request.retain());                  //2
+        } else {
+            if (HttpHeaders.is100ContinueExpected(request)) {
+                send100Continue(ctx);                               //3
+            }
+
+            RandomAccessFile file = new RandomAccessFile(INDEX, "r");//4
+
+            HttpResponse response = new DefaultHttpResponse(request.getProtocolVersion(), HttpResponseStatus.OK);
+            response.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/html; charset=UTF-8");
+
+            boolean keepAlive = HttpHeaders.isKeepAlive(request);
+
+            if (keepAlive) {                                        //5
+                response.headers().set(HttpHeaders.Names.CONTENT_LENGTH, file.length());
+                response.headers().set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
+            }
+            ctx.write(response);                    //6
+
+            if (ctx.pipeline().get(SslHandler.class) == null) {     //7
+                ctx.write(new DefaultFileRegion(file.getChannel(), 0, file.length()));
+            } else {
+                ctx.write(new ChunkedNioFile(file.getChannel()));
+            }
+            ChannelFuture future = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);           //8
+            if (!keepAlive) {
+                future.addListener(ChannelFutureListener.CLOSE);        //9
+            }
+
+            file.close();
+        }
+    }
+
+    private static void send100Continue(ChannelHandlerContext ctx) {
+        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.CONTINUE);
+        ctx.writeAndFlush(response);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+            throws Exception {
+        Channel incoming = ctx.channel();
+        System.out.println("Client:"+incoming.remoteAddress()+"异常");
+        // 当出现异常就关闭连接
         cause.printStackTrace();
         ctx.close();
-    }
-
-    @Override
-    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
-        System.out.println("return msg:"+ msg.toString());
-// 传统的HTTP接入
-        if (msg instanceof FullHttpRequest) {
-            handleHttpRequest(ctx, ((FullHttpRequest) msg));
-// WebSocket接入
-        } else if (msg instanceof WebSocketFrame) {
-            System.out.println(handshaker.uri());
-            if("anzhuo".equals(ctx.attr(AttributeKey.valueOf("type")).get())){
-                handlerWebSocketFrame(ctx, (WebSocketFrame) msg);
-            }else{
-                handlerWebSocketFrame2(ctx, (WebSocketFrame) msg);
-            }
-        }
     }
 }
